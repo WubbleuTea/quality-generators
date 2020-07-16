3d915f4e96bfd38e7ee5b9a11e96721dccf50970
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-16 11:42:51 -0500

    set up

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
 // array of questions for user
 const questions = [
+    // title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the tite of your proect?'
+    },
+    // description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please add a detailed decription of the project.'
+    },
+    // table of contents
+    {
+        type: 'confirm',
+        name: 'table',
+        message: 'Do you need a table of contents?',
+        default: false,
+    },
+    // usage
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage?'
+    },
+    //license
+    {
+        type: 'confirm',
+        name: 'licenceConfirm',
+        message: 'Do you need to include a license?',
+        default: 'false',
+    },
+    {
+        type: 'input',
+        name: 'licence',
+        message: 'Provide licencing info:',
+        when: ({ licenceConfirm }) => licenceConfirm
+    }
 
 ];
 
@@ -14,3 +54,6 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+//need a table of contents
